ccf6d5c99be09c058d25b08b9cb79a55802dacb5
kingthe100t-lab <kingthe100t@gmail.com>
2026-03-28 20:37:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ html_app = f"""
     </style>
 </head>
 <body>
-    <div class="app-container flex flex-col gap-6">
+    <div class="app-container">
+    <div class="max-w-[1400px] mx-auto flex flex-col gap-6 w-full">
         <h1 class="text-3xl font-black flex items-center gap-4 neon-text uppercase m-0">
             <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M21 16v-2l-8-5V3.5C13 2.67 12.33 2 11.5 2S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5L21 16z" fill="#0088ff"/>
@@ -222,7 +223,7 @@ html_app = f"""
             </div>
         </div>
     </div>
-
+</div>
     <script>
         // Data injected from Python Server
         const spots = {df_spots.to_json(orient="records")};
