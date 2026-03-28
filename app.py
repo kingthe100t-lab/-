b8a37da0771663cb767e5395c2fc7fe00597cef5
kingthe100t-lab <kingthe100t@gmail.com>
2026-03-29 00:53:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ html_app = f"""
         }}
 
         function getPlaneSvg(heading) {{
-            return `<div class="ghost-marker" style="width:44px;height:44px;"><svg width="44" height="44" style="filter:${{getShadowFilter()}} drop-shadow(0 0 6px rgba(129,236,255,0.6)); transition:filter 0.3s;"><g transform="rotate(${{heading}} 12 12)"><path d="M21 16v-2l-8-5V3.5C13 2.67 12.33 2 11.5 2S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5L21 16z" fill="#81ecff"/></g></svg></div>`;
+            return `<div class="ghost-marker" style="width:80px;height:80px;"><svg width="80" height="80" style="filter:${{getShadowFilter()}} drop-shadow(0 0 6px rgba(129,236,255,0.6)); transition:filter 0.3s;"><g transform="rotate(${{heading}} 12 12)"><path d="M21 16v-2l-8-5V3.5C13 2.67 12.33 2 11.5 2S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5L21 16z" fill="#81ecff"/></g></svg></div>`;
         }}
 
         function getCameraSvg(sel) {{
