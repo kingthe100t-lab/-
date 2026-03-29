c4f89171b953a6776ff58170ac64aa03b423dc8f
kingthe100t-lab <kingthe100t@gmail.com>
2026-03-29 17:43:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -340,12 +340,7 @@ html_app = f"""
                     return;
                 }}
                 
-                const res = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${{apiKey}}`, {{
-                    method: 'POST',
-                    headers: {{ 'Content-Type': 'application/json' }},
-                    body: JSON.stringify({{ contents: [{{ parts: [{{ text: prompt }}] }}] }})
-                }});
-                
+                const res = await fetch(url)
                 const data = await res.json(); 
                 
                 // API側からエラーが返ってきた場合
